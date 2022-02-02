63db422d7afa7cdf811f0dd3fa35d5cf0bcc2e00
K.Kiyokawa <koichi20110068@gmail.com>
2022-02-02 14:26:13 +0900

    feat: add counter

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text(String(count))
+                    .padding()
+                    .frame(width: 200.0)
+                Button("+1", action: increment)
+                    .accessibilityAddTraits([.isButton])
+                    .padding(.all)
+                    .accessibilityLabel("increment")
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                Button("-1", action: decrement)
+                    .padding(.all)
+                    .accessibilityLabel("decrement")
+                    .foregroundColor(Color.white)
+                    .background(Color.red)
+            }
+            Button("Add counter", action: addCounter)
+        }
+    }
+    
+    
+    func increment() {
+        count += 1
+    }
+    
+    func decrement() {
+        if (count > 0) { count -= 1 }
+    }
+    
+    func addCounter() {
+        // TODO:
     }
 }
 
