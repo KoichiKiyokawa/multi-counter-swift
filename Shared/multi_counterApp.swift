3d3e38ba78936e70be8466eed8b23a400a23af4f
K.Kiyokawa <koichi20110068@gmail.com>
2022-02-02 13:23:09 +0900

    Initial Commit

---
+++ Shared/multi_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_counterApp.swift
+//  Shared
+//
+//  Created by 清川航一 on 2022/02/02.
+//
+
+import SwiftUI
+
+@main
+struct multi_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
